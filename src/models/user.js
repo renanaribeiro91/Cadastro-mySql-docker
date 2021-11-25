e1d8c328c7ead27d84cedd9f08bae0c7aade9760
renanaribeiro91 <renan.admribeiro@gmail.com>
2021-11-24 21:19:52 -0300

    criando tabelas e modelos

---
+++ src/models/user.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+const database = require('../db');
+ 
+const Cliente = database.define('users', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+}, {
+    timestamps: false
+})
+ 
+module.exports = Users;
